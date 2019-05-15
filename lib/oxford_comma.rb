f5f9b55622a26d55379a2f223c3d5ed7dbd69a1a
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-15 06:46:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  
+if array.length==1 
+  return "#{array[0]}"
+  
+elsif array.length==2 
+    return "#{array[0]} and  #{array[1]}"
 
+else
+  return array.map {|i| "'" + i.to_s + "'" }.join(",")}
+  
+  end
 end
+end
+
+ 
