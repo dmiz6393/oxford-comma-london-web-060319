d01cd7953ea16d5850dac1c6c65a1eb87fe434ea
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-15 06:49:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,7 @@ elsif array.length==2
     return "#{array[0]} and  #{array[1]}"
 
 else
- array.map do |i| 
-   return ("'" + i.to_s + "'").join(",")
+ array.map {|i| "'" + i.to_s + "'" }.join(",")
   
   end
 end
